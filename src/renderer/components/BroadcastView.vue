b1b0777beb55d413a65e19f92445d3995b9eef7b
Maxwell Smith <max.smith@mclmediagroup.com>
2021-08-03 00:12:57 -0500

    add oseg

--- src/renderer/components/BroadcastView.vue
+++ src/renderer/components/BroadcastView.vue
@@ -25,13 +25,65 @@
         </div>
       </div>
     </nav>
-    
+    <!-- The main content of the page is the broadcasting software, it will contain a sidebar with a list of Scenes (each scene has a image, title, and icons toadd, edit, and remove the scene). and the bottom half of the side bar will list the propeties of the selected scene. The second section will have a preview window the bottom half having a Storeline (a storeline has a timeline track that can have Scenes placed on to be added to the preview video. The third section will have Output window that shows a modifed version of preview. Below the output window will be the audio mixing section )  -->
+    <div class="container">
+      <div class="row">
+        <div class="col-md-3">
+          <div class="sidebar">
+              <h4>Available Scenes</h4>
+              <div>
+                <ul class="list-group">
+                  <li class="list-group-item" v-for="scene in scenes" v-bind:key="scene.id">
+                    <image class="img-responsive" :src="scene.image" width="100%"></image>
+                    <h4 class="list-group-item-heading">{{scene.title}}</h4>
+                    <!-- Icons to push to preview, edit, and remove the scene -->
+                    <a class="btn btn-default" href="#" role="button" @click="pushScene(scene)">
+                      <i class="fa fa-plus"></i>
+                    </a>
+                    <a class="btn btn-default" href="#" role="button" @click="editScene(scene)">
+                      <i class="fa fa-pencil"></i>
+                    </a>
+                    <a class="btn btn-default" href="#" role="button" @click="removeScene(scene)">
+                      <i class="fa fa-trash"></i>
+                    </a>
+                  </li>
+                </ul>
+              </div>
+
+          </div>
+        </div>
+
+                        
+                    
+                                            
+            
 </div>   
+</div>
+
 </template>
 
 <script>
 export default {
+  name: 'BroadcastView',
+  data () {
+    return {
+      scenes: [],
+      oseg: undefined
+    }
+  },
+  methods: {
+    pushScene (scene) {
 
+    },
+    setupOSEG () {
+      //this.oseg = new OSEG();
+
+  }
+
+},
+  created () {
+    this.setupOSEG();
+  }
 }
 </script>
 
--- src/renderer/router/index.js
+++ src/renderer/router/index.js
@@ -1,5 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import 'bootstrap'
+import 'bootstrap/dist/css/bootstrap.min.css'
 
 Vue.use(Router)
 
